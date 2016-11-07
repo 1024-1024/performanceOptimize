26b4ee5284d9938f549e55d8556c64d21a2b3b73
zhangweilong <zhangwl.freedom@gmail.com>
2016-11-07 16:07:54 +0800

    性能优化

--- app/src/main/java/com/zwl/performanceoptimize/MainActivity.java
+++ app/src/main/java/com/zwl/performanceoptimize/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CommUtil commUtil = CommUtil.getInstance(this);
+//        CommUtil commUtil = CommUtil.getInstance(this);
 
     }
 }
