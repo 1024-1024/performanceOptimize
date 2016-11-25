91707050ec6a42624c80926e453c7d6b12e24fa4
zhangweilong <zhangwl.freedom@gmail.com>
2016-11-25 10:47:54 +0800

    klog

--- app/src/main/java/com/zwl/performanceoptimize/MainActivity.java
+++ app/src/main/java/com/zwl/performanceoptimize/MainActivity.java
@@ -3,6 +3,11 @@ package com.zwl.performanceoptimize;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.socks.library.KLog;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,7 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CommUtil commUtil = CommUtil.getInstance(this);
-
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("key1", "value1");
+            jsonObject.put("key2", "value2");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        KLog.json(jsonObject.toString());
+        KLog.json(jsonObject.toString());
+        KLog.json(jsonObject.toString());
+        KLog.json(jsonObject.toString());
     }
 }
